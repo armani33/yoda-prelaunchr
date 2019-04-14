eb480248e08d6a633d7cf4874c283fb87c1e1002
armand chariot <armand@chariot.be>
2019-04-14 22:19:22 +0200

    generate admin_user with gem and configurate it

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module YodaPrelaunchr
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    # decides whether the prelaunch campaign has ended or not
+    config.ended = ENV['CAMPAIGN_ENDED'].to_s == 'false'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  root to: 'users#new'
+
+  post 'users/create' => 'users#create'
+  get 'refer-a-friend' => 'users#refer'
+  get 'privacy-policy' => 'users#policy'
+
 end
