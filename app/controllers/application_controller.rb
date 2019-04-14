98baa29ee3c495126486a9ffecaf976dbd75522a
armand chariot <armand@chariot.be>
2019-04-14 22:31:19 +0200

    comment a lot in controller to solve heroku logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_action :ref_to_cookie
+  # before_action :ref_to_cookie
 
   def mobile_device?
     if session[:mobile_param]
@@ -13,17 +13,17 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def ref_to_cookie
-    campaign_ended = Rails.application.config.ended
-    return if campaign_ended || !params[:ref]
+  # def ref_to_cookie
+  #   campaign_ended = Rails.application.config.ended
+  #   return if campaign_ended || !params[:ref]
 
-    unless User.find_by_referral_code(params[:ref]).nil?
-      h_ref = { value: params[:ref], expires: 1.week.from_now }
-      cookies[:h_ref] = h_ref
-    end
+  #   unless User.find_by_referral_code(params[:ref]).nil?
+  #     h_ref = { value: params[:ref], expires: 1.week.from_now }
+  #     cookies[:h_ref] = h_ref
+  #   end
 
-    user_agent = request.env['HTTP_USER_AGENT']
-    return unless user_agent && !user_agent.include?('facebookexternalhit/1.1')
-    redirect_to proc { url_for(params.except(:ref)) }
-  end
+  #   user_agent = request.env['HTTP_USER_AGENT']
+  #   return unless user_agent && !user_agent.include?('facebookexternalhit/1.1')
+  #   redirect_to proc { url_for(params.except(:ref)) }
+  # end
 end
