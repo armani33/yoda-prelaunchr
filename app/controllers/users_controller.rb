c749f956c67e14510f05fbd558352bb8c9d71020
armand chariot <armand@chariot.be>
2019-04-15 16:59:51 +0200

    trying to resolve user.save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,35 +14,31 @@ class UsersController < ApplicationController
     end
   end
 
-  # def create
-  #   ref_code = cookies[:h_ref]
-  #   email = params[:user][:email]
-  #   @user = User.new(email: email)
-  #   @user.referrer = User.find_by_referral_code(ref_code) if ref_code
+  def create
+    ref_code = cookies[:h_ref]
+    @user = User.new(user_params)
+    @user.referrer = User.find_by_referral_code(ref_code) if ref_code
+    if @user.save
+      cookies[:h_email] = { value: @user.email }
+      redirect_to refer_a_friend_path
+    else
+      logger.info("Error saving user with email, #{user_params}")
+      redirect_to root_path, alert: 'Something went wrong 1!'
+    end
+  end
 
-  #   if @user.save
-  #     cookies[:h_email] = { value: @user.email }
-  #     redirect_to '/refer-a-friend'
-  #   else
-  #     logger.info("Error saving user with email, #{email}")
-  #     redirect_to root_path, alert: 'Something went wrong!'
-  #   end
-  # end
+  def refer
 
-  # def refer
-  #   @bodyId = 'refer'
-  #   @is_mobile = mobile_device?
+    @user = User.find_by_email(cookies[:h_email])
 
-  #   @user = User.find_by_email(cookies[:h_email])
-
-  #   respond_to do |format|
-  #     if @user.nil?
-  #       format.html { redirect_to root_path, alert: 'Something went wrong!' }
-  #     else
-  #       format.html # refer.html.erb
-  #     end
-  #   end
-  # end
+    respond_to do |format|
+      if @user.nil?
+        format.html { redirect_to root_path, alert: 'Something went wrong!' }
+      else
+        format.html # refer.html.erb
+      end
+    end
+  end
 
   def policy
   end
@@ -53,16 +49,20 @@ class UsersController < ApplicationController
 
   private
 
-  # def skip_first_page
-  #   return if Rails.application.config.ended
+  def user_params
+    params.require(:user).permit(:email)
+  end
 
-  #   email = cookies[:h_email]
-  #   if email && User.find_by_email(email)
-  #     redirect_to '/refer-a-friend'
-  #   else
-  #     cookies.delete :h_email
-  #   end
-  # end
+  def skip_first_page
+    return if Rails.application.config.ended
+
+    email = cookies[:h_email]
+    if email && User.find_by_email(email)
+      redirect_to refer_a_friend_path
+    else
+      cookies.delete :h_email
+    end
+  end
 
   # def handle_ip
   #   # Prevent someone from gaming the site by referring themselves.
