4be265b0814d360b8e57e6acf82ed2be34e94707
armand chariot <armand@chariot.be>
2019-04-18 13:52:07 +0200

    add pregress images, style progress bar part of website

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,28 +16,28 @@ class User < ApplicationRecord
   REFERRAL_STEPS = [
     {
       'count' => 5,
-      'html' => 'Shave<br>Cream',
+      'html' => '7 Rides<br>Free',
       'class' => 'two',
       # 'image' =>  ActionController::Base.helpers.asset_path(
       #   'refer/cream-tooltip@2x.png')
     },
     {
       'count' => 10,
-      'html' => 'Truman Handle<br>w/ Blade',
+      'html' => '15 Rides<br>+ 15 minutes Free',
       'class' => 'three',
       # 'image' => ActionController::Base.helpers.asset_path(
       #   'refer/truman@2x.png')
     },
     {
       'count' => 25,
-      'html' => 'Winston<br>Shave Set',
+      'html' => '1 Week<br>abonnement Free',
       'class' => 'four',
       # 'image' => ActionController::Base.helpers.asset_path(
       #   'refer/winston@2x.png')
     },
     {
       'count' => 50,
-      'html' => 'One Year<br>Free Blades',
+      'html' => '1 Month<br>abonnement Free',
       'class' => 'five',
       # 'image' => ActionController::Base.helpers.asset_path(
       #   'refer/blade-explain@2x.png')
