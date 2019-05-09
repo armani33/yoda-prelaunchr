218d660925baa12f4e4e54b2bc6f5d7a23d804dc
armand chariot <armand@chariot.be>
2019-05-09 16:02:36 +0200

    try to put only the image path

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
       'count' => 25,
       'html' => '40 Rides<br> + 80 minutes Free<br>+ 1 T-shirt',
       'class' => 'four',
-      'image' => asset_path('images/reduc_3.png')
+      'image' => 'images/reduc_3.png'
     },
     {
       'count' => 50,
